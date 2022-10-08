18457e51e9e633df2d811dfdfa9e08013724270e
Diego <DiegoClindsey@gmail.com>
2022-10-08 17:17:51 +0200

    Implement class gameImpl

---
+++ scoreboard/src/app/models/gameImpl.ts
@@ -0,0 +1,19 @@
+import { Game } from "../interfaces/game";
+import { Team } from "../interfaces/team";
+
+export class GameImpl implements Game{
+  awayTeamScore: number = 0
+  localTeamScore: number = 0
+  awayTeam:Team
+  localTeam:Team
+
+  constructor(localTeam:Team,awayTeam:Team){
+    this.awayTeam = awayTeam
+    this.localTeam = localTeam
+  }
+
+  updateScore(localTeamScore: number, awayTeamScore: number): void {
+      this.localTeamScore = localTeamScore
+      this.awayTeamScore = awayTeamScore
+  }
+}
