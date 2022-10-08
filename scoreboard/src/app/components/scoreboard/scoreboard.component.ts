a495eb4a64f2973ae1d8ec6c76b2009531daffff
Diego <DiegoClindsey@gmail.com>
2022-10-08 17:00:18 +0200

    Create scoreboard component
    * Previsualize a game
    * Add some tests
    * Modify scoreboard interface to include the game to update

---
+++ scoreboard/src/app/components/scoreboard/scoreboard.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/app/interfaces/game';
+import { Team } from 'src/app/interfaces/team';
+import {ScoreBoard} from '../../interfaces/score-board'
+
+@Component({
+  selector: 'app-scoreboard',
+  templateUrl: './scoreboard.component.html',
+  styleUrls: ['./scoreboard.component.css']
+})
+export class ScoreboardComponent implements OnInit, ScoreBoard{
+
+
+  @Input games : Game[]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public startGame(localTeam: Team, awayTeam: Team): void {
+
+  }
+
+  public finishGame(game: Game): void {
+
+  }
+
+  public updateScore(localTeamScore: number, awayTeamScore: number, game:Game): void {
+
+  }
+
+}
