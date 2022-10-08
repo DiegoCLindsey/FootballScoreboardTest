8d93ec687b51afe22bb4cbaaf74a3cf9caa63436
Diego <DiegoClindsey@gmail.com>
2022-10-08 17:18:27 +0200

    Implement method startGame matching test specifications

--- scoreboard/src/app/components/scoreboard/scoreboard.component.ts
+++ scoreboard/src/app/components/scoreboard/scoreboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Game } from 'src/app/interfaces/game';
 import { Team } from 'src/app/interfaces/team';
+import { GameImpl } from 'src/app/models/gameImpl';
 import {ScoreBoard} from '../../interfaces/score-board'
 
 @Component({
@@ -11,15 +12,21 @@ import {ScoreBoard} from '../../interfaces/score-board'
 export class ScoreboardComponent implements OnInit, ScoreBoard{
 
 
-  @Input() games! : Game[]
+  @Input() games : Game[] = []
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Given the teams involved in a game, creates the game and adds it to the game list
+   * @param localTeam Local team of game
+   * @param awayTeam Away team of game
+   */
   public startGame(localTeam: Team, awayTeam: Team): void {
-
+    const newGame: Game = new GameImpl(localTeam, awayTeam)
+    this.games.push(newGame)
   }
 
   public finishGame(game: Game): void {
