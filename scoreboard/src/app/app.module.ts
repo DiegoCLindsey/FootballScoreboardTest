e1a8457d13121744b71934783aa5685464b11f66
Diego <DiegoClindsey@gmail.com>
2022-10-09 20:34:51 +0200

    Add implementation of scoreboard on app.component

--- scoreboard/src/app/app.module.ts
+++ scoreboard/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './components/game/game.component';
+import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
+    ScoreboardComponent,
   ],
   imports: [
     BrowserModule,
